113d5d8eebd9ee7b6c54fe0d58ae66f04257f42a
Marzia Culotta <[marzy90@gmail.com]>
2021-12-27 20:11:32 +0100

    feat: add jokes component + joke service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JokesComponent } from './core/components/jokes/jokes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JokesComponent
   ],
   imports: [
     BrowserModule,
